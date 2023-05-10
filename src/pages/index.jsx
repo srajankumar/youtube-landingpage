f681a38bba2fb7904b2728d0722c1b888b7dd4d8
srajankumar <kumarsrajan02@gmail.com>
2023-05-10 20:59:37 +0530

    dynamic component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,32 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Header from "./components/Header";
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+// import Header from "./components/Header";
 import Sidebar from "../pages/components/Sidebar";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return (
-    <main>
-      <div className="fixed">
-        <Header />
-        <Sidebar />
-      </div>
-    </main>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <div className="fixed">
+//         <Header />
+//         <Sidebar />
+//       </div>
+//     </main>
+//   );
+// }
+
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(() => import("./components/Header"), {
+  ssr: false,
+});
+
+const HomePage = () => (
+  <div>
+    <DynamicComponentWithNoSSR />
+    <Sidebar />
+  </div>
+);
+
+export default HomePage;
