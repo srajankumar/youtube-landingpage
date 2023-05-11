0c9a5b777cd7e4484473d79844fa2d329a7f3711
srajankumar <kumarsrajan02@gmail.com>
2023-05-11 11:18:02 +0530

    search-bar done

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,61 +1,17 @@
 import React from "react";
-import { Sidenav, initTE } from "tw-elements";
 
-initTE({ Sidenav });
-
-function App() {
+function Header() {
   return (
-    <div>
-      {/* <!-- Sidenav --> */}
-      <div className="container mx-auto flex flex-wrap pl-2 pt-4 flex-row items-center">
-        <div className="flex pt-2 pb-5">
-          <button
-            class=" inline-block rounded bg-primary px-5 py-0 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-            data-te-sidenav-toggle-ref
-            data-te-target="#sidenav-1"
-            aria-controls="#sidenav-1"
-            aria-haspopup="true"
-          >
-            <span class="block [&>svg]:h-6 [&>svg]:w-6 [&>svg]:text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-          </button>
-          <div className="pt-1 px-3">
-            <img className="h-5 w-auto" src="/img/yt-sidebar.png" />
-          </div>
-        </div>
-      </div>
-      <nav
-        id="sidenav-1"
-        class="absolute left-0 top-15 z-[1035] h-auto w-60 -translate-x-full overflow-hidden bg-black shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0"
-        data-te-sidenav-init
-        data-te-sidenav-hidden="false"
-        data-te-sidenav-position="absolute"
-      >
-        {/* <button
-          class="mt-10 inline-block rounded bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-          data-te-sidenav-toggle-ref
-          data-te-target="#sidenav-1"
-          aria-controls="#sidenav-1"
-          aria-haspopup="true"
-        >
-          <span class="block [&>svg]:h-5 [&>svg]:w-5 [&>svg]:text-white">
+    <div className="fixed w-full h-full">
+      <div id="menuToggle" className="">
+        <input className="togglee" type="checkbox" />
+        <div className="fixed top-0 left-2 container mx-auto flex flex-wrap pl-3.5 pt-4 flex-row items-center">
+          <div className="flex pt-1 pb-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              class="h-5 w-5"
+              class="h-7 w-7"
             >
               <path
                 fill-rule="evenodd"
@@ -63,11 +19,58 @@ function App() {
                 clip-rule="evenodd"
               />
             </svg>
-          </span>
-        </button> */}
-        <ul class="relative m-0 list-none px-[0.2rem]" data-te-sidenav-menu-ref>
-          <div className="pl-2">
-            {" "}
+            <div className="pt-1 px-6">
+              <img className="h-5 w-auto" src="/img/yt-sidebar.png" />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full left-1/4 pl-14 pt-4 fixed top-0">
+          <input
+            type="text"
+            id="footer-field"
+            name="footer-field"
+            placeholder="Search"
+            class="w-[34rem] bg-[#121212] px-5 bg-opacity-50 rounded-l-3xl border border-[#303030] focus:ring-1 focus:bg-transparent focus:ring-indigo-400 focus:border-0 text-base outline-none text-gray-100 leading-8 transition-colors duration-200 ease-in-out"
+          />
+          <button class="inline-flex text-white bg-[#202020] border-l-0 border border-[#303030] py-2 px-5 focus:outline-none rounded-r-3xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.612 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3l-1.4 1.4ZM9.5 14q1.875 0 3.188-1.313T14 9.5q0-1.875-1.313-3.188T9.5 5Q7.625 5 6.312 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14Z"
+              />
+            </svg>
+          </button>
+        </div>
+        <ul
+          id="menu"
+          class=" pl-8 pt-5 pr-10 w-auto relative m-0 list-none"
+          data-te-sidenav-menu-ref
+        >
+          <div>
+            <div className="container mx-auto flex flex-wrap pl-3.5 pt-5 flex-row items-center">
+              <div className="flex pt-1 pb-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="h-7 w-7"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <div className="pt-1 px-6">
+                  <img className="h-5 w-auto" src="/img/yt-sidebar.png" />
+                </div>
+              </div>
+            </div>
             <li class="relative">
               <a
                 class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
@@ -129,7 +132,7 @@ function App() {
           <div className="p-1" />
           <hr />
           <div className="p-1" />
-          <div className="pl-2">
+          <div>
             <li class="relative">
               <a
                 class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
@@ -244,6 +247,33 @@ function App() {
                 <div className="font-semibold px-5">Playlist 1</div>
               </a>
             </li>
+          </div>
+          <div className="p-1" />
+          <hr />
+          <div className="p-1" />
+          <div>
+            <li class="relative">
+              <a
+                class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                data-te-sidenav-link-ref
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m5.23 2.25l.43-1.11A1 1 0 0 1 6.59.5h.82a1 1 0 0 1 .93.64l.43 1.11l1.46.84l1.18-.18a1 1 0 0 1 1 .49l.4.7a1 1 0 0 1-.08 1.13l-.73.93v1.68l.75.93a1 1 0 0 1 .08 1.13l-.4.7a1 1 0 0 1-1 .49l-1.18-.18l-1.46.84l-.43 1.11a1 1 0 0 1-.93.64h-.84a1 1 0 0 1-.93-.64l-.43-1.11l-1.46-.84l-1.18.18a1 1 0 0 1-1-.49l-.4-.7a1 1 0 0 1 .08-1.13L2 7.84V6.16l-.75-.93a1 1 0 0 1-.08-1.13l.4-.7a1 1 0 0 1 1-.49l1.18.18ZM5 7a2 2 0 1 0 2-2a2 2 0 0 0-2 2Z"
+                  />
+                </svg>
+                <div className="font-semibold px-5">Settings</div>
+              </a>
+            </li>
             <li class="relative">
               <a
                 class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
@@ -257,80 +287,41 @@ function App() {
                 >
                   <path
                     fill="currentColor"
-                    d="M3 10h11v2H3zm0-4h11v2H3zm0 8h7v2H3zm13-1v8l6-4z"
+                    d="M11 16h2v2h-2zm1-14C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5c0-2.21-1.79-4-4-4z"
                   />
                 </svg>
-                <div className="font-semibold px-5">Playlist 2</div>
+                <div className="font-semibold px-5">Help</div>
+              </a>
+            </li>
+            <li class="relative">
+              <a
+                class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                data-te-sidenav-link-ref
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M6.455 19L2 22.5V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H6.455ZM4 18.385L5.763 17H20V5H4v13.385ZM11 13h2v2h-2v-2Zm0-6h2v5h-2V7Z"
+                  />
+                </svg>
+                <div className="font-semibold px-5">Send feedback</div>
               </a>
             </li>
           </div>
           <div className="p-1" />
           <hr />
-          <div className="p-1" />
-          <div className="pl-6 py-2">Subscriptions</div>
-          <div className="pl-2">
-            <li class="relative">
-              <a
-                class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
-                data-te-sidenav-link-ref
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="m11.5 14.5l7-4.5l-7-4.5v9ZM6 18V2h16v16H6Zm2-2h12V4H8v12Zm-6 6V6h2v14h14v2H2ZM8 4v12V4Z"
-                  />
-                </svg>
-                <div className="font-semibold px-5">Name 1</div>
-              </a>
-            </li>
-            <li class="relative">
-              <a
-                class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
-                data-te-sidenav-link-ref
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="m11.5 14.5l7-4.5l-7-4.5v9ZM6 18V2h16v16H6Zm2-2h12V4H8v12Zm-6 6V6h2v14h14v2H2ZM8 4v12V4Z"
-                  />
-                </svg>
-                <div className="font-semibold px-5">Name 2</div>
-              </a>
-            </li>
-            <li class="relative">
-              <a
-                class="flex h-12 cursor-pointer items-center truncate rounded-[10px] px-4 py-0 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
-                data-te-sidenav-link-ref
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="m11.5 14.5l7-4.5l-7-4.5v9ZM6 18V2h16v16H6Zm2-2h12V4H8v12Zm-6 6V6h2v14h14v2H2ZM8 4v12V4Z"
-                  />
-                </svg>
-                <div className="font-semibold px-5">Name 2</div>
-              </a>
-            </li>
+          <div className="text-xs font-semibold py-7 text-gray-300 pl-4 pr-0">
+            © 2023 Google LLC
           </div>
         </ul>
-      </nav>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Header;
