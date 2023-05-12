88cb4a9118ebf055a58fa1032ac9de8c56f44557
srajankumar <kumarsrajan02@gmail.com>
2023-05-12 18:27:54 +0530

    components dir replaced

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
-import Body from "./components/Body";
+import Header from "../components/Header";
+import Body from "../components/Body";
 
 const Home = () => {
   return (
